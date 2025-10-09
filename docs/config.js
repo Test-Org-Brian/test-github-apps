7617277a42325c3d18f6e9fb7b52997f18276222
Brian Powell <brian@powell.place>
2025-10-09 13:52:25 -0700

    Fix callback

--- docs/config.js
+++ docs/config.js
@@ -3,7 +3,6 @@ window.APP_CONFIG = {
         org: 'test-org-brian',
         repo: 'test-github-apps'
     },
-    // Add other config as needed
     features: {
         autoRefresh: true,
         debugging: false
