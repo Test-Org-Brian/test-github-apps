1c7e658ad0cc2aec8b5658a388775ec7b957dcee
Brian Powell <brian@powell.place>
2025-10-22 16:05:17 -0700

    Testing out custom gh pages workflow

--- docs/config.template.js
+++ docs/config.template.js
@@ -1,6 +1,5 @@
 window.APP_CONFIG = {
     github: {
-        enterprise: '__GITHUB_ENTERPRISE__',
         org: '__GITHUB_ORG__',
         repo: '__GITHUB_REPO__',
         token: '__GITHUB_TOKEN__'
